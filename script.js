c8729ac55c52274dc8e07062e1a6563ece10439e
s-t-e-p <stephanenjemen@gmail.com>
2025-09-06 14:00:49 -0400

    Ajout de la nouvelle fonctionnalité

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 ﻿function afficherMessage() {
     alert('Nos experts sont là pour vous accompagner dans vos projets informatiques.');
 }
+function afficherAPropos() {
+    const section = document.getElementById('a-propos');
+    section.style.display = 'block';
+}
